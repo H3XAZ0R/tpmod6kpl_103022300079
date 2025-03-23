e9c717b1ecdae810200d022fa7a2f3a43393290f
Syahdanmansizk <syahdanmk1234@gmail.com>
2025-03-23 23:47:54 +0700

    Add files via upload

---
+++ tpmod6_103022300079/tpmod6_103022300079/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("tpmod6_103022300079")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("tpmod6_103022300079")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("e25f60e7-c24d-4fd2-ae65-a81b74a1f142")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ tpmod6_103022300079/tpmod6_103022300079/SayaTubeVideo.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace tpmod6_103022300079
+{
+    public class SayaTubeVideo
+    {
+        private int id;
+        private string title;
+        private int playCount;
+
+        public SayaTubeVideo(string title)
+        {
+            Debug.Assert(!string.IsNullOrEmpty(title) && title.Length <= 100, "Judul video tidak boleh null dan panjang maksimal 100 karakter.");
+
+            Random rand = new Random();
+            this.id = rand.Next(10000, 100000);
+            this.title = title;
+            this.playCount = 0;
+        }
+
+        public void IncreasePlayCount(int count)
+        {
+            Debug.Assert(count >= 0 && count <= 10000000, "Jumlah play count harus antara 0 dan 10.000.000.");
+
+            try
+            {
+                checked
+                {
+                    this.playCount += count;
+                }
+            }
+            catch (OverflowException e)
+            {
+                Console.WriteLine("Error: Terjadi overflow pada jumlah play count.");
+            }
+        }
+
+        public void PrintVideoDetails()
+        {
+            Console.WriteLine("Video ID: " + this.id);
+            Console.WriteLine("Title: " + this.title);
+            Console.WriteLine("Play Count: " + this.playCount);
+        }
+
+        public static void Main(string[] args)
+        {
+            SayaTubeVideo video = new SayaTubeVideo("Tutorial Design By Contract - [NAMA_PRAKTIKAN]");
+
+            video.IncreasePlayCount(5);
+            video.PrintVideoDetails();
+
+            Console.WriteLine("\nMenguji batas input play count:");
+            try
+            {
+                video.IncreasePlayCount(10000001);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error: " + e.Message);
+            }
+
+            Console.WriteLine("\nMenguji overflow dengan loop:");
+            for (int i = 0; i < 500; i++)
+            {
+                video.IncreasePlayCount(int.MaxValue / 2);
+            }
+        }
+    }
+}
